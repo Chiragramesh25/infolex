2cde1a23e001d71c6df21098166817e76f1892a3
Aviral Sharma <aviral2020it015@abesit.edu.in>
2023-09-21 22:36:41 +0530

    node-server

--- server/server.js
+++ server/server.js
@@ -1,35 +1,18 @@
-require('dotenv').config();
-
-const express = require("express");
-const app = express();
-const port = 5000;
+const express = require('express')
 const cors = require('cors');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const crypto = require('crypto');
+const root = require('./routes/root')
+const { connectDb } = require("./database");
 
-app.use(cors());
+const app = express()
+const port = 8080
+
+connectDb()
+
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use("/", root)
 
-const accountSid = process.env.ACCOUNT_SID;
-const authToken = process.env.AUTH_TOKEN;
-const smskey =  process.env.SMS_SECRET_KEY;
-let twilioNum = process.env.TWILIO_PHONE_NUMBER;
-
-const client = require('twilio')(accountSid, authToken);
-
-app.post("/sendOTP", (req, res)=>{
-        const { phone } = req.body;
-        const otp = Math.floor(100000 + Math.random() * 900000);
-        const ttl = 2*60*1000;
-        let expires = Date.now();
-        expires+=ttl;
-        const data = `${phone}.${otp}.${expires}`;
-        const hash = crypto.createHmac('sha256', smskey).update(data).digest('hex');
-        const fullHash = `${hash}.${expires}`;
-});
-
-app.listen(port, ()=>{
-    console.log(`listening on ${port}`);
-});
+app.listen(port, () => {
+    console.log(`Server running on Port ${port}.`)
+})
